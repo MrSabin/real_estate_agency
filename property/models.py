dc12aae8b453a82ddc5fec42c3a1f9b6f47a71f9
MrSabin <sabin.21011986@gmail.com>
2022-11-02 20:53:38 +0300

    Sorted model attributes according to codestyle

--- property/models.py
+++ property/models.py
@@ -9,10 +9,8 @@ class Flat(models.Model):
     created_at = models.DateTimeField(
         "Когда создано объявление", default=timezone.now, db_index=True
     )
-
     description = models.TextField("Текст объявления", blank=True)
     price = models.IntegerField("Цена квартиры", db_index=True)
-
     town = models.CharField(
         "Город, где находится квартира", max_length=50, db_index=True
     )
@@ -26,22 +24,19 @@ class Flat(models.Model):
     floor = models.CharField(
         "Этаж", max_length=3, help_text="Первый этаж, последний этаж, пятый этаж"
     )
-
     rooms_number = models.IntegerField("Количество комнат в квартире", db_index=True)
     living_area = models.IntegerField(
         "количество жилых кв.метров", null=True, blank=True, db_index=True
     )
-
     has_balcony = models.NullBooleanField("Наличие балкона", db_index=True)
     active = models.BooleanField("Активно-ли объявление", db_index=True)
     construction_year = models.IntegerField(
         "Год постройки здания", null=True, blank=True, db_index=True
     )
+    likes = models.ManyToManyField(User, related_name="liked_by", blank=True, verbose_name="Кто лайкнул")
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
-
-    likes = models.ManyToManyField(User, related_name="liked_by", blank=True, verbose_name="Кто лайкнул")
 
 
 class Complaint(models.Model):
